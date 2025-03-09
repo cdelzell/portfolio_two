34700a7c2c0b0c53f4ffd5fd43775a20a81dcbf9
cdelzell <claradelzell@gmail.com>
2025-03-09 15:58:14 -0500

    fun little emoji buttons

--- src/home/components/experiences/experiences.jsx
+++ src/home/components/experiences/experiences.jsx
@@ -1,17 +1,52 @@
+import { useState } from "react";
 import Experience from "./experience";
 import "./experience.css";
 
 function Experiences() {
+  const [hideTrain, setHideTrain] = useState(false);
+  const [hideStar, setHideStar] = useState(false);
+  const [hideHammer, setHideHammer] = useState(false);
+
+  const handleClick = (hideObject, setHideObject) => {
+    if (!hideObject) {
+      setHideObject(true);
+    }
+  };
+
   return (
     <div className="experiences" id="experience">
-      <Experience
-        image="ticket_to_ride"
-        name="Ticket to Ride 🚂"
-        blurb="I learned a ton about how to create a full stack web application, and got to work with a great team to finish the product!"
-        details="a full stack web application in React and Typescript with multi-player support."
-      />
-      <Experience image="bright" name="A Brighter Future ⭐" />
-      <Experience image="portfolio_original" name="My first portfolio 🔨" />
+      {hideTrain ? (
+        <Experience
+          image="ticket_to_ride"
+          name="Ticket to Ride 🚂"
+          blurb="I learned a ton about how to create a full stack web application, and got to work with a great team to finish the product!"
+          details="a full stack web application in React and Typescript with multi-player support."
+        />
+      ) : (
+        <button onClick={() => handleClick(hideTrain, setHideTrain)}>🚂</button>
+      )}
+
+      {hideStar ? (
+        <Experience image="bright" name="A Brighter Future ⭐" />
+      ) : (
+        <button
+          onClick={() => handleClick(hideStar, setHideStar)}
+          style={{ fontSize: "15vw" }}
+        >
+          ⭐
+        </button>
+      )}
+
+      {hideHammer ? (
+        <Experience image="portfolio_original" name="My first portfolio 🔨" />
+      ) : (
+        <button
+          onClick={() => handleClick(hideHammer, setHideHammer)}
+          style={{ fontSize: "15vw" }}
+        >
+          🔨
+        </button>
+      )}
     </div>
   );
 }
